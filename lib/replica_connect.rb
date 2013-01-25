33c43cd287ec03343cef810e3cd696e4d4b8513b
Richard Boyle <rmichaelboyle@gmail.com>
2013-01-24 19:01:04 -0500

    werks

--- lib/replica_connect.rb
+++ lib/replica_connect.rb
@@ -1,31 +1,52 @@
 require "replica_connect/version"
-require 'sequel'
+require 'active_record'
 
 module ReplicaConnect
   class Connection
 
-    def initialize(username=nil, password=nil)
-      @username = username || get_username_from_user
-      @password = password || get_password_from_user
+    def initialize
+      if File.exists?('rc_config.yml')
+        file = YAML.load_file('rc_config.yml')
+        configs = file['rc']
+        @username = configs['username']
+        @password = configs['password']
+        @db_name = configs['db']
+        @host = configs['host']
+        @adapter = configs['adapter']
+      else
+        f = File.new('rc_config.yml')
+        f.puts "rc:"
+        @username =  get_from_user('username',f)
+        @password =  get_from_user('password',f)
+        @db_name  =  get_from_user('db',f)
+        @host     =  get_from_user('host',f)
+        @adapter  =  get_from_user('adapter',f)
+        f.close
+      end
     end
 
+    def connect
+      ActiveRecord::Base.establish_connection(
+        :adapter => @adapter, 
+        :host => @host, 
+        :database => @db_name, 
+        :username => @username, 
+        :password => @password).connection
+    end
 
-    def list
-      puts "#{@username} and #{@password}"
+    def clear_env
+      ['rc_username', 'rc_password', 'rc_db', 'rc_host', 'rc_adapter'].each do |ev|
+        %x("unset {ev}")
+      end
     end
 
     private
 
-    def get_username_from_user
-      puts "Please enter your username:"
-      gets.chomp
+    def get_from_user(param, file)
+      puts "Please enter your #{param}"
+      p = gets.chomp
+      f.puts "  #{param}: #{p}"
+      p
     end
-
-    def get_password_from_user
-      puts "Please enter your password:"
-      gets.chomp
-    end
-
   end
-  
 end
